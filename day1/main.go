7c3bc1f18f2d610343039bdfab719b5e34f7e474
alumonabenaiah@gmail.com <alumonabenaiah71@gmail.com>
2022-01-31 23:07:15 +0100

    added day1 and 2 leaning exp

--- day1/main.go
+++ day1/main.go
@@ -1,4 +1,6 @@
 // a package is a way to group functions, and  it's made up of all the files in the same directory
+// every program is part of a package. We define this using the package keyword. In this example, the program belongs to the main package.
+
 package main
 
 import "fmt"
---
+++ day2/main.go
@@ -0,0 +1,60 @@
+// VARIABLES
+
+package main
+
+import "fmt"
+
+// varaiables
+// in go we have diff ways of declearing variables
+// 1. initial type eg ( var a string = "welcome" )
+// 2. inferred type eg ( var a = "welcome" )
+// or without the var keyword ( x := "welcome" )
+
+func main() {
+
+	// var n1 string = "benrobo" // type is string
+	// var n2 = "john"
+	// n3 := "doe"
+
+	// fmt.Println(n1, n2, n3)
+
+	// If a varaiable is declared without a initial value, the value of the variable would be the default type of that varaible
+
+	// var a string
+	// var b int
+	// var c bool
+
+	// fmt.Println(a)
+	// fmt.Println(b)
+	// fmt.Println(c)
+
+	// var x string
+	// x = "ben"
+
+	// print(a)
+
+	// // Multiple declearation
+	// a, b, c := "a", "b", "c"
+
+	// print(a)
+	// print(b)
+	// print(c)
+
+	// Multiple varaiable in a block
+	// var (
+	// 	a = 2
+	// 	b = "ben"
+	// 	c = true
+	// )
+
+	// fmt.Println(a)
+	// fmt.Println(b)
+	// fmt.Println(c)
+
+	// Go varaiable naming rules
+
+	myName_2 := "benrobo"
+
+	fmt.Println(myName_2)
+
+}
