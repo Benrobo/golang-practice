afba59068b207eb516c74de05254cf49970e7b19
benrobo <alumonabenaiah71@gmail.com>
2022-10-08 03:29:11 -0700

    updated

--- var.go
+++ var.go
@@ -2,7 +2,7 @@
 
 package main
 
-// import "fmt"
+import "fmt"
 
 // // varaiables
 // // in go we have diff ways of declearing variables
@@ -10,52 +10,51 @@ package main
 // // 2. inferred type eg ( var a = "welcome" )
 // // or without the var keyword ( x := "welcome" )
 
-// func main() {
+func main() {
 
-	
-// 	// var n1 string = "benrobo" // type is string
-// 	// var n2 = "john"
-// 	// n3 := "doe"
+	var n1 string = "benrobo" // type is string
+	var n2 = "john"
+	n3 := "doe"
 
-// 	// fmt.Println(n1, n2, n3)
+	fmt.Println(n1, n2, n3)
 
-// 	// If a varaiable is declared without a initial value, the value of the variable would be the default type of that varaible
+	// If a varaiable is declared without a initial value, the value of the variable would be the default type of that varaible
 
-// 	// var a string
-// 	// var b int
-// 	// var c bool
+	// var a string
+	// var b int
+	// var c bool
 
-// 	// fmt.Println(a)
-// 	// fmt.Println(b)
-// 	// fmt.Println(c)
+	// fmt.Println(a)
+	// fmt.Println(b)
+	// fmt.Println(c)
 
-// 	// var x string
-// 	// x = "ben"
+	var x string
+	x = "ben"
 
-// 	// print(a)
+	print(x)
 
-// 	// // Multiple declearation
-// 	// a, b, c := "a", "b", "c"
+	// // Multiple declearation
+	a, b, c := "a", "b", "c"
 
-// 	// print(a)
-// 	// print(b)
-// 	// print(c)
+	print(a)
+	print(b)
+	print(c)
 
-// 	// Multiple varaiable in a block
-// 	// var (
-// 	// 	a = 2
-// 	// 	b = "ben"
-// 	// 	c = true
-// 	// )
+	// Multiple varaiable in a block
+	// var (
+	// 	a = 2
+	// 	b = "ben"
+	// 	c = true
+	// )
 
-// 	// fmt.Println(a)
-// 	// fmt.Println(b)
-// 	// fmt.Println(c)
+	fmt.Println(a)
+	fmt.Println(b)
+	fmt.Println(c)
 
-// 	// Go varaiable naming rules
+	// Go varaiable naming rules
 
-// 	myName_2 := "benrobo"
+	myName_2 := "benrobo"
 
-// 	fmt.Println(myName_2)
+	fmt.Println(myName_2)
 
-// }
+}
