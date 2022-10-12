7c0daf019a025165fe49fad71836ed3f6122da67
benrobo <alumonabenaiah71@gmail.com>
2022-10-12 13:12:46 -0700

    updated

--- decision/main.go
+++ decision/main.go
@@ -5,17 +5,15 @@ import "fmt"
 // in golang, we can make decision using if...then and switch..case
 
 func main() {
-	var (
-		a = 45
-		b = 23
-	)
 
-	// if...else
-
-	if a > b && a != b {
-		fmt.Println("A is greater than B")
+	a := 14
+	b := 14
+	if a < b {
+		fmt.Println("a is less than b.")
+	} else if a > b {
+		fmt.Println("a is more than b.")
 	} else {
-		fmt.Println(`A is smaller than B`)
+		fmt.Println("a and b are equal.")
 	}
 
 	// switch..case
@@ -29,5 +27,35 @@ func main() {
 		fmt.Println(`active`)
 	default:
 		fmt.Print(`neither active nor inactive`)
+	}
+
+	day := 4
+
+	switch day {
+	case 1:
+		fmt.Println("Monday")
+	case 2:
+		fmt.Println("Tuesday")
+	case 3:
+		fmt.Println("Wednesday")
+	case 4:
+		fmt.Println("Thursday")
+	case 5:
+		fmt.Println("Friday")
+	case 6:
+		fmt.Println("Saturday")
+	case 7:
+		fmt.Println("Sunday")
+	}
+
+	// nested if..else  statement
+	num := 20
+	if num >= 10 {
+		fmt.Println("Num is more than 10.")
+		if num > 15 {
+			fmt.Println("Num is also more than 15.")
+		}
+	} else {
+		fmt.Println("Num is less than 10.")
 	}
 }
