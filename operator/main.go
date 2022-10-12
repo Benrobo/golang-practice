0446f8fa232899786208003f704de911c6ae24fc
benrobo <alumonabenaiah71@gmail.com>
2022-10-12 12:57:21 -0700

    added operator

---
+++ operator/main.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	// && and
+	// || or
+	// ! not
+	var (
+		a = 34
+		b = 20
+	)
+
+	fmt.Println(a > b)
+	fmt.Println(a >= b)
+	fmt.Println(a > b && a != b)
+
+}
